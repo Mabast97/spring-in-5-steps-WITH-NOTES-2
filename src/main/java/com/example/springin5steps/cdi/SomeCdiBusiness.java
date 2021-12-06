8711543e90480027a89768368b0979184e9f4031
Mabast <mabasthashm1@gmail.com>
2021-12-06 16:34:47 +0300

    Initial commit

---
+++ src/main/java/com/example/springin5steps/cdi/SomeCdiBusiness.java
@@ -0,0 +1,24 @@
+package com.example.springin5steps.cdi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+//@Component  // in spring
+@Named  // in CDI, instead of @Component
+public class SomeCdiBusiness {
+
+    @Autowired  // in spring
+    @Inject  // in CDI, instead of @Autowired
+    private SomeCdiDao someCdiDao;
+
+    public SomeCdiDao getSomeCdiDao() {
+        return someCdiDao;
+    }
+
+    public void setSomeCdiDao(SomeCdiDao someCdiDao) {
+        this.someCdiDao = someCdiDao;
+    }
+}

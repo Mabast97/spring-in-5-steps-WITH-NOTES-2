22d0e8952b809063b4b51ca16a45fb43ce832e38
Mabast <mabasthashm1@gmail.com>
2022-02-01 16:01:35 +0300

    first commit

--- src/main/java/com/example/springin5steps/SpringIn5StepsBasicApplication.java
+++ src/main/java/com/example/springin5steps/SpringIn5StepsBasicApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+
 @SpringBootApplication // this annotation automatically scans the package and the sub packages of the package where this is in for beans
 public class SpringIn5StepsBasicApplication {
 
